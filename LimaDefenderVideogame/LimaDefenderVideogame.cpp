f2eb1542371247f70a98dfdd33bf3204a9975ed1
AlvaroDTU <alvarosteven69@gmail.com>
2025-11-16 13:14:09 -0500

    En proceso de agregar que los vecinos desaparezcan cuando nos atacan

--- LimaDefenderVideogame/LimaDefenderVideogame.cpp
+++ LimaDefenderVideogame/LimaDefenderVideogame.cpp
@@ -11,12 +11,15 @@ struct Enemigo {
     int y;
     int tipo;
     int vida;
+    bool atacando;
+    int cooldownataque;
 };
 struct Vecino {
     bool activo;
     int x;
     int y;
     int cooldown;
+    int vida;
 };
 struct Bala {
     bool activa;
@@ -28,10 +31,10 @@ int main() {
 	srand(time(nullptr));
 	Console::SetWindowSize(190, 50);
 	Console::CursorVisible = false;
-	bool inicio = true, juego = false, nivel[] = { 0, 0, 0, 0 };
+    bool inicio = true, juego = false, gameOver = false, nivel[] = { 0, 0, 0, 0 };
 	while (true) {
 		int xopciones = 1, yopciones = 1;
-		inicio = true, juego = false;
+        inicio = true, juego = false, gameOver = false;
 		imprime_mapa();
 		imprime_titulo(55, 3);
 		imprime_play(60, 29);
@@ -109,9 +112,10 @@ int main() {
 			}
 		}
 		while (juego) {
-		
-			_sleep(250);
+            slod0();
 			while (_getch() != 13) {}
+            slod1();
+            while (_getch() != 13) {}
 			bool barra_seleccion[3] = { false,false,false };
 			nivel_surco();
             puntosVecinales();
@@ -120,7 +124,7 @@ int main() {
             //Constantes por nivel
 			const int numLineas = 4;
             const int numColumnas = 3;
-			const int maxEnemigos = 2;
+			const int maxEnemigosLinea = 2;
             const int MAX_BALAS = 20;
             int yLineas[numLineas] = { 16, 25, 34, 43 };
             
@@ -132,16 +136,17 @@ int main() {
             const int TIEMPO_ENTRE_DISPAROS = 50;
             int puntosV = 100;
 
-            Enemigo enemigos[numLineas][maxEnemigos];
+            Enemigo enemigos[numLineas][maxEnemigosLinea];
             Vecino vecinos[numLineas][numColumnas];
             Bala balas[MAX_BALAS];
             //Inicializar enemigos
             for (int l = 0; l < numLineas; l++) {
-                for (int s = 0; s < maxEnemigos; s++) {
+                for (int s = 0; s < maxEnemigosLinea; s++) {
                     enemigos[l][s].activo = false;
                     enemigos[l][s].x = 0;
                     enemigos[l][s].y = 0;
                     enemigos[l][s].tipo = 0;
+                    enemigos[l][s].vida = 3;
                 }
             }
             //Inicializar vecinos
@@ -151,6 +156,7 @@ int main() {
                     vecinos[l][c].cooldown = 0;
                     vecinos[l][c].x = 0;
                     vecinos[l][c].y = 0;
+                    vecinos[l][c].vida = 5;
                 }
 
             //Inicializar balas
@@ -163,6 +169,7 @@ int main() {
 			//INICIO ANIMACIONES NIVEL 1
             while (nivel[0])
             {
+                // SISTEMA DE PUNTOS VECINALES
                 gotoxy(6, 1);
                 Console::BackgroundColor = ConsoleColor::White;
                 Console::ForegroundColor = ConsoleColor::Black;
@@ -180,7 +187,7 @@ int main() {
                     {
                         int lineaRandom = rand() % numLineas;
 
-                        for (int s = 0; s < maxEnemigos; s++)
+                        for (int s = 0; s < maxEnemigosLinea; s++)
                         {
                             if (!enemigos[lineaRandom][s].activo)
                             {
@@ -188,9 +195,11 @@ int main() {
                                 enemigos[lineaRandom][s].x = 181;
                                 enemigos[lineaRandom][s].y = yLineas[lineaRandom];
                                 enemigos[lineaRandom][s].tipo = 1 + rand() % 3;
-
+                                enemigos[lineaRandom][s].vida = 3;
+                                enemigos[lineaRandom][s].atacando = false;
+                                enemigos[lineaRandom][s].cooldownataque = 0;
                                 enemigosGenerados++;
-                                spawnCooldown = 80 + rand() % 40;
+                                spawnCooldown = 100;
                                 break;
                             }
                         }
@@ -204,18 +213,22 @@ int main() {
                 //----------------------------------------------------------
                 for (int l = 0; l < numLineas; l++)
                 {
-                    for (int s = 0; s < maxEnemigos; s++)
+                    for (int s = 0; s < maxEnemigosLinea; s++)
                     {
                         if (!enemigos[l][s].activo) continue;
 
                         borrar_enemigo((int)enemigos[l][s].x, enemigos[l][s].y);
 
-                        enemigos[l][s].x -= 0.2;
+                        switch (enemigos[l][s].tipo) {
+                            case 1: enemigos[l][s].x -= 0.2; break;
+                            case 2: enemigos[l][s].x -= 1.0; break;
+                            case 3: enemigos[l][s].x -= 0.5; break;
+                        }
 
-                        if (enemigos[l][s].x <= 50) {
+                        if (enemigos[l][s].x <= 42) {
                             enemigos[l][s].activo = false;
-                            enemigosEliminados++;
-                            continue;
+                            gameOver = true;
+                            break;
                         }
 
                         switch (enemigos[l][s].tipo) {
@@ -225,15 +238,14 @@ int main() {
                         }
                     }
                 }
-
-
+                if (gameOver) break;
                 //----------------------------------------------------------
                 // 3. DETECTAR ENEMIGOS EN CADA LÍNEA
                 //----------------------------------------------------------
                 bool enemigoEnLinea[numLineas] = { false,false,false,false };
 
                 for (int l = 0; l < numLineas; l++)
-                    for (int s = 0; s < maxEnemigos; s++)
+                    for (int s = 0; s < maxEnemigosLinea; s++)
                         if (enemigos[l][s].activo) enemigoEnLinea[l] = true;
 
 
@@ -281,37 +293,31 @@ int main() {
                     for (int c = 0; c < numColumnas; c++)
                     {
                         if (!vecinos[l][c].activo) continue;
-
                         if (!enemigoEnLinea[l]) {
                             vecinos[l][c].cooldown = 0;
                             continue;
                         }
-
                         if (vecinos[l][c].cooldown > 0) {
                             vecinos[l][c].cooldown--;
                             continue;
                         }
-
                         // Crear bala
                         for (int i = 0; i < MAX_BALAS; i++) {
                             if (!balas[i].activa) {
-
                                 balas[i].activa = true;
                                 balas[i].x = vecinos[l][c].x + 8;
                                 balas[i].y = vecinos[l][c].y + 2;
                                 balas[i].linea = l;
-
                                 break;
                             }
                         }
-
                         vecinos[l][c].cooldown = TIEMPO_ENTRE_DISPAROS;
                     }
                 }
 
 
                 //----------------------------------------------------------
-                // 7. MOVER BALAS + COLISIONES
+                // 7. MOVER BALAS 
                 //----------------------------------------------------------
                 for (int i = 0; i < MAX_BALAS; i++)
                 {
@@ -319,24 +325,28 @@ int main() {
 
                     borrar_bala((int)balas[i].x, balas[i].y);
                     balas[i].x += 1.0;
-
+                    //COLISION MAPA
                     if (balas[i].x > 185) {
                         balas[i].activa = false;
                         continue;
                     }
 
-                    for (int s = 0; s < maxEnemigos; s++)
+                    //COLISION CON EL ENEMIGO
+                    for (int s = 0; s < maxEnemigosLinea; s++)
                     {
                         if (!enemigos[balas[i].linea][s].activo) continue;
 
-                        if (balas[i].x >= enemigos[balas[i].linea][s].x - 2 && balas[i].x <= enemigos[balas[i].linea][s].x + 6)
+                        if (balas[i].x >= enemigos[balas[i].linea][s].x && balas[i].x <= enemigos[balas[i].linea][s].x + 8)
                         {
-                            
-                            borrar_enemigo((int)enemigos[balas[i].linea][s].x, enemigos[balas[i].linea][s].y);
-                            enemigos[balas[i].linea][s].activo = false;
-                            puntosV += 20;
-                            enemigosEliminados++;
                             balas[i].activa = false;
+                            enemigos[balas[i].linea][s].vida--;
+                            if (enemigos[balas[i].linea][s].vida == 0) {
+                                borrar_enemigo((int)enemigos[balas[i].linea][s].x, enemigos[balas[i].linea][s].y);
+                                enemigos[balas[i].linea][s].activo = false;
+                                puntosV += 20;
+                                enemigosEliminados++;
+                                enemigos[balas[i].linea][s].vida = 3;
+                            }
                             break;
                         }
                     }
@@ -356,8 +366,6 @@ int main() {
 
                 dibujar_prota(xprota, yprota);
                 casilla(xcasilla, ycasilla);
-
-
                 //----------------------------------------------------------
                 // 9. HUD
                 //----------------------------------------------------------
@@ -371,6 +379,8 @@ int main() {
                     enemigosEliminados == maxEnemigosNivel)
                 {
                     nivel[0] = false;
+                    nivel[1] = true;
+                    gameOver = false;
                     break;
                 }
 
@@ -378,6 +388,12 @@ int main() {
                 // 11. FPS
                 //----------------------------------------------------------
                 _sleep(30);
+            }
+            if (gameOver) {
+                juego = false;
+                Perdiste();
+                while (_getch() == 13) {}
+                break;
             }
 		}
 	}
